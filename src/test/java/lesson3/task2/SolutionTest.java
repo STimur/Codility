3ddc44920d999d0319f1307f91ae6e5d00d4d815
Timur <timur.salakhetdinov@gmail.com>
2017-09-19 02:52:17 +0300

    clean tests

--- src/test/java/lesson3/task2/SolutionTest.java
+++ src/test/java/lesson3/task2/SolutionTest.java
@@ -1,19 +1,38 @@
 package lesson3.task2;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
+    private Solution solution;
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    private void assertMinDiff(int[] tape, int expected) {
+        assertEquals(expected, solution.solution(tape));
+    }
+
+    private int[] intArray(int... ints) {
+        int[] array = new int[ints.length];
+        for (int i = 0; i < ints.length; i++)
+            array[i] = ints[i];
+        return array;
+    }
+
     @Test
     public void twoElementsArray() throws Exception {
-        assertEquals(0, new Solution().solution(new int[2]));
-        assertEquals(1, new Solution().solution(new int[]{1, 2}));
-        assertEquals(1, new Solution().solution(new int[]{2, 1}));
+        assertMinDiff(intArray(0, 0), 0);
+        assertMinDiff(intArray(1, 2), 1);
+        assertMinDiff(intArray(2, 1), 1);
     }
 
     @Test
     public void threeElementsArray() throws Exception {
-        assertEquals(0, new Solution().solution(new int[]{2, 1, 1}));
+        assertMinDiff(new int[]{2, 1, 1}, 0);
     }
 }
