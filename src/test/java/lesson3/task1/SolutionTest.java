e9745e048782c56a80fe4746ddfdcba486d60fda
Timur <timur.salakhetdinov@gmail.com>
2017-09-18 06:58:40 +0300

    clean tests

--- src/test/java/lesson3/task1/SolutionTest.java
+++ src/test/java/lesson3/task1/SolutionTest.java
@@ -1,18 +1,26 @@
 package lesson3.task1;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
+    private Solution solution;
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
     @Test
     public void noJumpsNeeded() throws Exception {
-        assertEquals(0, new Solution().solution(1, 1, 1));
+        assertEquals(0, solution.solution(1, 1, 1));
     }
 
     @Test
     public void oneJumpNeeded() throws Exception {
-        assertEquals(1, new Solution().solution(1, 2, 1));
-        assertEquals(1, new Solution().solution(1, 3, 2));
+        assertEquals(1, solution.solution(1, 2, 1));
+        assertEquals(1, solution.solution(1, 3, 2));
     }
 }
