21ea4f7c8501d9081a7af35cebd308b78524d476
Timur <timur.salakhetdinov@gmail.com>
2017-09-19 03:00:35 +0300

    clean code

--- src/main/java/lesson3/task2/Solution.java
+++ src/main/java/lesson3/task2/Solution.java
@@ -2,15 +2,22 @@ package lesson3.task2;
 
 public class Solution {
     public int solution(int[] A) {
-        int diff = 0;
-        if (A.length == 2)
-            diff = A[0] - A[1];
-        if (A.length == 3) {
-            if (A[0] > A[2])
-                diff = A[0] - A[1] - A[2];
-            else
-                diff = A[0] + A[1] - A[2];
+        int diff = Integer.MAX_VALUE;
+        int newDiff = 0;
+        int P = 1;
+        int N = A.length;
+        while (P < N) {
+            int leftSum = 0;
+            for (int i = 0; i < P; i++)
+                leftSum += A[i];
+            int rightSum = 0;
+            for (int i = P; i < N; i++)
+                rightSum += A[i];
+            newDiff = Math.abs(leftSum - rightSum);
+            if (newDiff < diff)
+                diff = newDiff;
+            P++;
         }
-        return diff > 0 ? diff : (-1) * diff;
+        return diff;
     }
 }
