056c3ac896383e803c7a4332d36c95d4548f6698
Timur <timur.salakhetdinov@gmail.com>
2017-09-19 03:55:54 +0300

    optimize performance to O(n) time complexity

--- src/main/java/lesson3/task2/Solution.java
+++ src/main/java/lesson3/task2/Solution.java
@@ -2,22 +2,17 @@ package lesson3.task2;
 
 public class Solution {
     public int solution(int[] A) {
-        int diff = -1, newDiff = 0, P = 1, N = A.length;
-        while (P < N) {
-            newDiff = Math.abs(sum(A, 0, P) - sum(A, P, N));
-            if (diff == -1)
-                diff = newDiff;
-            else if (newDiff < diff)
-                diff = newDiff;
-            P++;
-        }
-        return diff;
-    }
+        int i = 0, j = A.length - 1, leftSum = A[i], rightSum = A[j];
 
-    private int sum(int[] A, int from, int to) {
-        int sum = 0;
-        for (int i = from; i < to; i++)
-            sum += A[i];
-        return sum;
+        while (j - i != 1)
+            if (leftSum <= rightSum) {
+                i++;
+                leftSum += A[i];
+            } else {
+                j--;
+                rightSum += A[j];
+            }
+
+        return Math.abs(leftSum - rightSum);
     }
 }
