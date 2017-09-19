a5fd148360b43b683944c296ee36817cdfb3b039
Timur <timur.salakhetdinov@gmail.com>
2017-09-19 05:44:50 +0300

    refactor to recursion

--- src/main/java/lesson3/task2/Solution.java
+++ src/main/java/lesson3/task2/Solution.java
@@ -2,31 +2,25 @@ package lesson3.task2;
 
 public class Solution {
     public int solution(int[] A) {
-        int i = 0, j = A.length - 1, leftSum = A[i], rightSum = A[j];
+        if (A.length == 2)
+            return Math.abs(A[0]-A[1]);
 
-        while (j - i != 1) {
-            if (leftSum >= 0) {
-                if (leftSum <= rightSum) {
-                    i++;
-                    leftSum += A[i];
-                } else if (rightSum >= 0) {
-                    j--;
-                    rightSum += A[j];
-                } else {
-                    j--;
-                    rightSum += A[j];
-                }
-            } else {
-                if (leftSum > rightSum) {
-                    i++;
-                    leftSum += A[i];
-                } else {
-                    j--;
-                    rightSum += A[j];
-                }
-            }
+        int i = 0; int j = A.length - 1;
+        int diffCandidate = A[i] - A[j];
+        i++; j--;
+        return Math.abs(count(diffCandidate, A, i, j));
+    }
+
+    private int count(int diffCandidate, int[] a, int i, int j) {
+        if (i == j)
+            return Math.min(Math.abs(diffCandidate + a[i]), Math.abs(diffCandidate - a[j]));
+        if (Math.abs(diffCandidate + a[i]) <= Math.abs(diffCandidate - a[j])) {
+            diffCandidate += a[i];
+            i++;
+        } else {
+            diffCandidate -= a[j];
+            j--;
         }
-
-        return Math.abs(leftSum - rightSum);
+        return count(diffCandidate, a, i, j);
     }
 }
