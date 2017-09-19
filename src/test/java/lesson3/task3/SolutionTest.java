4b11c45dc7ce8472a5954e68a023f3aaebee8611
Timur <timur.salakhetdinov@gmail.com>
2017-09-20 00:40:50 +0300

    add acceptance test

--- src/test/java/lesson3/task3/SolutionTest.java
+++ src/test/java/lesson3/task3/SolutionTest.java
@@ -31,4 +31,13 @@ public class SolutionTest {
         assertMissedElement(intArray(1), 2);
         assertMissedElement(intArray(2), 1);
     }
+
+    @Test
+    public void acceptance() throws Exception {
+        assertMissedElement(intArray(2, 3, 1, 5), 4);
+        assertMissedElement(intArray(2, 3, 4, 5), 1);
+        assertMissedElement(intArray(2, 4, 1, 5), 3);
+        assertMissedElement(intArray(2, 3, 1, 4), 5);
+        assertMissedElement(intArray(5, 3, 1, 4), 2);
+    }
 }
