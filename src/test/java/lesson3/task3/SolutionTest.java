5ba72451b8f08bfeb04d4d6de5e8a66b70d1e626
Timur <timur.salakhetdinov@gmail.com>
2017-09-20 00:36:36 +0300

    clean tests

--- src/test/java/lesson3/task3/SolutionTest.java
+++ src/test/java/lesson3/task3/SolutionTest.java
@@ -1,18 +1,34 @@
 package lesson3.task3;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
+    private Solution solution;
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    private int[] intArray(int... ints) {
+        return ints;
+    }
+
+    private void assertMissedElement(int[] array, int missedElement) {
+        assertEquals(missedElement, solution.solution(array));
+    }
+
     @Test
     public void emptyArray() throws Exception {
-        assertEquals(1, new Solution().solution(new int[0]));
+        assertMissedElement(intArray(), 1);
     }
 
     @Test
     public void oneElementArray() throws Exception {
-        assertEquals(2, new Solution().solution(new int[]{1}));
-        assertEquals(1, new Solution().solution(new int[]{2}));
+        assertMissedElement(intArray(1), 2);
+        assertMissedElement(intArray(2), 1);
     }
 }
