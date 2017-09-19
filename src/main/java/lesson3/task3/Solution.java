de88acf5888f0d3c9a72a1eb173f504931793cac
Timur <timur.salakhetdinov@gmail.com>
2017-09-20 01:53:10 +0300

    fix overflow in algorithm

--- src/main/java/lesson3/task3/Solution.java
+++ src/main/java/lesson3/task3/Solution.java
@@ -3,9 +3,9 @@ package lesson3.task3;
 public class Solution {
     public int solution(int[] A) {
         int n = A.length;
-        int missed = (n+1) * (n+2) / 2;
+        long missed = (n + 1L) * (n + 2) / 2;
         for (int i : A)
             missed -= i;
-        return missed;
+        return (int) missed;
     }
 }
